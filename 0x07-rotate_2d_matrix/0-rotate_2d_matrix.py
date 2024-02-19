4b7b74ddbf75cf5372919757086707b0db84be26
elyse502 <elyseniyibizi502@gmail.com>
2024-02-19 12:57:21 +0000

    Rotate a given matrix 90 degrees clockwise

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module 0-rotate_2d_matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
+
+    Args:
+                    matrix (_type_): _description_
+    """
+    N = len(matrix)
+
+    for i in range(N):
+        for j in range(i, N):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(N):
+        for j in range(N // 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][N - 1 - j]
+            matrix[i][N - 1 - j] = temp
